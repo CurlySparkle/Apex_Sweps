a606df0539afdb33d6ff476f4193124ae22c44b3
May Lian <twilight_sparkle13@hotmail.com>
2022-05-07 23:55:11 -0400

    ssss
    hi

--- lua/autorun/arccw_apex_ammocounter.lua
+++ lua/autorun/arccw_apex_ammocounter.lua
@@ -24,4 +24,35 @@ if CLIENT then
                 end
         end
     } )
+    matproxy.Add( {
+        name = "ArcCW_AMMO_BAR",
+        init = function( self, mat, values )
+            self.ResultTo = values.resultvar
+            self.Prefix = values.prefixstring
+        end,
+        bind = function( self, mat, ent )
+                local Place = self.ResultTo
+                local texture
+
+                if LocalPlayer():GetActiveWeapon() then
+                    local OurWeapon = LocalPlayer():GetActiveWeapon()
+                    if OurWeapon.ArcCW then
+                        local BarCount = tostring(OurWeapon:Clip1())
+
+                        if OurWeapon:Clip1() > 21 then
+                            BarCount = 21
+                        end
+
+                        texture = self.Prefix .. BarCount
+                        
+                        print(texture)
+                    end
+                end
+
+                if texture then
+                    mat:SetTexture( "$basetexture", texture )
+                end
+        end
+    } )
 end
+
