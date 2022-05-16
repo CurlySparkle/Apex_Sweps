675cbc68312d0886d3daaba85156c519e19542fd
May Lian <twilight_sparkle13@hotmail.com>
2022-05-16 03:37:00 -0400

    r1
    forgot to include the firemodes on the r301 lol

--- lua/weapons/arccw_apex_r301.lua
+++ lua/weapons/arccw_apex_r301.lua
@@ -323,6 +323,12 @@ SWEP.Animations = {
             {p = 100, s = "weapons/foley/Weapon_Inspect_Foley_SMG_End_V2_2ch_01.wav", t = 316 / 30}
     },
     },
+    ["1_to_2"] = {
+        Source = "firemode1",
+    },
+    ["2_to_1"] = {
+        Source = "firemode2",
+    },
     ["reload"] = {
         Source = "reload",
         TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
