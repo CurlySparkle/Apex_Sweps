a7703c870979127f02b10bccfe00766f54491ef4
TheOnly8Z <888888zombies@gmail.com>
2022-05-24 19:14:58 -0500

    energy ammo explosion

--- lua/entities/arccw_ammo_apex_energy.lua
+++ lua/entities/arccw_ammo_apex_energy.lua
@@ -13,6 +13,25 @@ ENT.AmmoType = "apex_energy"
 ENT.AmmoCount = 20
 ENT.PickupSound = "items/Pickups_Ammo_Energy_V1_1ch_01.wav"
 
+ENT.DetonationRadius = 256
+ENT.DetonationDamage = 75
+
 if engine.ActiveGamemode() == "terrortown" then
     ENT.AmmoCount = ENT.AmmoCount * 1.5
+end
+
+function ENT:Detonate(wet, attacker)
+    local explode = ents.Create( "info_particle_system" )
+    explode:SetKeyValue( "effect_name", "tfa_apex_arcstar_explode" )
+    explode:SetOwner( self.Owner )
+    explode:SetPos( self:GetPos() )
+    explode:Spawn()
+    explode:Activate()
+    explode:Fire( "start", "", 0 )
+    explode:Fire( "kill", "", 30 )
+
+    self:EmitSound("weapons/grenades/arcstar/explo_star_close_2ch_v1_0" .. math.random(1, 3) .. ".wav", 100, 100, 1, CHAN_AUTO)
+
+    util.BlastDamage(self, attacker, self:GetPos(), self.DetonationRadius, self.DetonationDamage)
+    self:Remove()
 end
