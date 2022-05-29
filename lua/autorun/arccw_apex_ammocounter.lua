ff2d316b3a534763046c13894f221d31434dfff3
Matheus Silagi <matsilagi2@hotmail.com>
2022-05-29 07:14:10 -0300

    Particle artpass + cleanup, CAR sight ammocounter

--- lua/autorun/arccw_apex_ammocounter.lua
+++ lua/autorun/arccw_apex_ammocounter.lua
@@ -121,5 +121,36 @@ if CLIENT then
                 end
         end
     } )
+	matproxy.Add({
+		name = "ArcCW_AMMO_CAR",
+		init = function(self, mat, values)
+			self.ResultTo = values.resultvar
+			self.Prefix = values.prefixstring
+		end,
+		bind = function(self, mat, ent)
+			local texture 
+            if LocalPlayer():GetActiveWeapon() then
+                local OurWeapon = LocalPlayer():GetActiveWeapon()
+                if OurWeapon.ArcCW then
+					local fullmag = OurWeapon:GetMaxClip1()
+					local curmag = OurWeapon:Clip1()
+					
+					if (curmag / fullmag) >= 0.60 then
+						texture = self.Prefix .. 3
+					elseif (curmag / fullmag) < 0.60 and (curmag / fullmag) > 0.30 then
+						texture = self.Prefix .. 2
+					elseif (curmag / fullmag) <= 0.30 and (curmag / fullmag) > 0.0 then
+						texture = self.Prefix .. 1
+					else
+						texture = self.Prefix .. 0
+					end
+					
+					if texture then
+						mat:SetTexture( "$basetexture", texture )
+					end
+				end
+			end
+		end
+	})
 end
 
