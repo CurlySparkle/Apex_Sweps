4c90163471f3b0f88474bfbb157b1eae5a768177
May Lian <twilight_sparkle13@hotmail.com>
2022-05-29 15:22:28 -0400

    sound stuff for melee

---
+++ lua/arccw/shared/sh_apex_sounds_melee.lua
@@ -0,0 +1,80 @@
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Hit.Default",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+    sound = {
+        "^weapons/melee/Imp_Player_MeleePunch_Default_1ch_v1_1.wav",
+		"^weapons/melee/Imp_Player_MeleePunch_Default_1ch_v1_2.wav",
+		"^weapons/melee/Imp_Player_MeleePunch_Default_1ch_v1_3.wav",
+		"^weapons/melee/Imp_Player_MeleePunch_Default_1ch_v1_4.wav",
+    }
+} )
+
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Hit.Flesh",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+--    pitch = {95, 105},
+    sound = {
+        "^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_1.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_2.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_3.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_4.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_5.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Hit_Flesh_1P_2ch_v1_6.wav",
+    }
+} )
+
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Swing.Elbow",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+--    pitch = {95, 105},
+    sound = {
+        "^weapons/melee/Pilot_Mvmt_Melee_Elbow_1P_2ch_v1_1.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Elbow_1P_2ch_v1_2.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Elbow_1P_2ch_v1_3.wav",
+    }
+} )
+
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Swing.Left",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+--    pitch = {95, 105},
+    sound = {
+        "^weapons/melee/Pilot_Mvmt_Melee_LeftHook_1P_2ch_v1_1.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_LeftHook_1P_2ch_v1_2.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_LeftHook_1P_2ch_v1_3.wav",
+    }
+} )
+
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Swing.Right",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+--    pitch = {95, 105},
+    sound = {
+        "^weapons/melee/Pilot_Mvmt_Melee_RightHook_1P_2ch_v1_1.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_RightHook_1P_2ch_v1_2.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_RightHook_1P_2ch_v1_3.wav",
+    }
+} )
+
+sound.Add( {
+    name = "ArcCW_APEX.Melee.Swing.Upper",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 120,
+--    pitch = {95, 105},
+    sound = {
+        "^weapons/melee/Pilot_Mvmt_Melee_Uppercut_1P_2ch_v1_1.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Uppercut_1P_2ch_v1_2.wav",
+		"^weapons/melee/Pilot_Mvmt_Melee_Uppercut_1P_2ch_v1_3.wav",
+    }
+} )
